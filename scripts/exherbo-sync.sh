b785e735178e02e00827b1d7926da7691e115d55
BlackYoup <a.lefebvre@outlook.fr>
2016-06-27 17:10:17 +0200

    exherbo-sync: fix bad if

--- scripts/exherbo-sync.sh
+++ scripts/exherbo-sync.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 exherbo-sync() {
-  if [ -n "$1"]; then
+  local repo=$1
+  local package=$2
+
+  if [[ -z $repo ]]; then
     echo "Syncs local repository"
     echo "Usage: exherbo-sync repo [package-to-resolve]"
     return 1
@@ -9,7 +12,7 @@ exherbo-sync() {
 
   cave sync -s local -r $(cd /home/arnaud/Dev/exherbo/repos/$1; rev=$(git rev-parse $(git branch | grep "\*" | cut -d' ' -f2)) && echo $rev) $1  
 
-  if [ -n "$2" ]; then
+  if [[ -n $package ]]; then
     cave resolve $2 -xz1
   fi
 }
