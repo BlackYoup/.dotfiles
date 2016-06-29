31ee5ddae7854df8d655eed49f8f8fb4f5c1c5b7
BlackYoup <a.lefebvre@outlook.fr>
2016-06-29 14:35:05 +0200

    exherbo-sync: remote trailing spaces

--- scripts/exherbo-sync.sh
+++ scripts/exherbo-sync.sh
@@ -10,7 +10,7 @@ exherbo-sync() {
     return 1
   fi
 
-  cave sync -s local -r $(cd /home/arnaud/Dev/exherbo/repos/$1; rev=$(git rev-parse $(git branch | grep "\*" | cut -d' ' -f2)) && echo $rev) $1  
+  cave sync -s local -r $(cd /home/arnaud/Dev/exherbo/repos/$1; rev=$(git rev-parse $(git branch | grep "\*" | cut -d' ' -f2)) && echo $rev) $1
 
   if [[ -n $package ]]; then
     cave resolve $2 -xz1
