c08c2a854e1a7e1bc652886bacae94844a2076fe
BlackYoup <a.lefebvre@outlook.fr>
2017-03-19 20:07:46 +0100

    Add a script to automatically format patch for exherbo contributions

---
+++ scripts/format-patch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+
+format-patch(){
+  local commitnbr=$1
+  local name=$2
+  local currentdir=${PWD##*/}
+  local autopatchpath=/etc/paludis/autopatch/${currentdir}
+
+  if [[ ! -d "${autopatchpath}" ]]; then
+    mkdir "${autopatchpath}"
+  fi
+
+  if [[ $name = "" ]]; then
+    name=$(git branch | grep "\*" | cut -d' ' -f2)
+  fi
+
+  local destfile=${autopatchpath}/${name}.patch
+
+  git format-patch -M -C --find-copies-harder --stdout HEAD~${commitnbr} > ${destfile}
+}
