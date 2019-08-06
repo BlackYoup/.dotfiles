6deb1eb368424aa0a400aabe36a412ccdaac41b0
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2019-08-06 12:17:17 +0200

    Add script to unset gdk scale

---
+++ scripts/unset-gdk-scale.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+focused=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .output')
+if [[ "${focused}" == "" ]]; then
+  exit 0
+fi
+
+output=$(i3-msg -t get_outputs | jq ".[] | select(.name==${focused})")
+
+if [[ "${output}" != "" ]]; then
+  width=$(echo -n "${output}" | jq '.rect | .width')
+  height=$(echo -n "${output}" | jq '.rect | .height')
+
+  if [[ "${width}" -lt 3840 ]] && [[ "${height}" -lt 2160 ]]; then
+    unset GDK_SCALE
+  fi
+fi
+
+"${@}"
